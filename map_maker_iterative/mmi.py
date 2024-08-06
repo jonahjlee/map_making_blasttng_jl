2d63488e231a9e140ba989826c2b249898a0cb74
James Burgoyne <thejaburjunk+github@gmail.com>
2024-08-06 13:13:17 -0700

    debug

--- map_maker_iterative/mmi.py
+++ map_maker_iterative/mmi.py
@@ -112,8 +112,6 @@ def main():
     # kids to use
     kids = dlib.findAllKIDs(dir_roach) # all in dir_roach; sorted
 
-    print(kids)
-
     # remove unused channels
     kids = [kid for kid in kids if int(kid) <= kid_max]
 
@@ -122,8 +120,6 @@ def main():
         kid_rejects = dlib.loadKidRejects(file_rejects)
         kids = [kid for kid in kids if kid not in kid_rejects]
     except: pass
-        
-    print(kids)
 
     # move ref kid so it's processed first
     # this is last so it raises an error if our ref has been removed
