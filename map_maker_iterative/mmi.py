3ff178982e2e8003d7afdbeae75c726a32f15742
lambpasta <jonahjacqueslee@gmail.com>
2025-01-21 13:07:37 -0800

    add breakpoint

--- map_maker_iterative/mmi.py
+++ map_maker_iterative/mmi.py
@@ -125,6 +125,8 @@ def main():
     # convert offsets in degrees to um on image plane
     x_um, y_um = mlib.offsetsTanProj(x_az, y_el, platescale)
 
+    import pdb; pdb.set_trace()
+
     # generate map bins and axes
     xx, yy, x_bins, y_bins, x_edges, y_edges = mlib.genMapAxesAndBins(
         x_um, y_um, x_bin, y_bin)
