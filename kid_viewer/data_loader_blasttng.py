a87ae66987cd6968aed5021d3fb8380f093fc103
lambpasta <jonahjacqueslee@gmail.com>
2025-02-12 16:01:44 -0800

    First draft: data_loader_blasttng.py

---
+++ kid_viewer/data_loader_blasttng.py
@@ -0,0 +1,38 @@
+# ============================================================================ #
+# data_loader.py
+#
+# Jonah Lee
+#
+# Data Loader
+# Load BLAST-TNG data from the HAA CCAT Control Computer into .npy files
+# for use in kid_viewer.py. Intended use is to
+# ============================================================================ #
+
+import sys
+import os
+import numpy as np
+# modify PATH environment variable to access modules in other directory
+sys.path.append(os.path.join(os.getcwd(), '..', 'map_maker_iterative'))
+from map_maker_iterative.mmi_config import RoachID, ScanPass
+from map_maker_iterative.mmi_roach import RoachPass
+
+
+def get_norm_df_dict(roach):
+    return {int(kid):roach.get_norm_kid_df(kid) for kid in roach.kids}
+
+def get_df_dict(roach):
+    return {int(kid):roach.get_kid_df(kid) for kid in roach.kids}
+
+
+if __name__ == '__main__':
+
+    roach = RoachPass(RoachID(1), ScanPass.ALL, use_rejects_file=False)
+    print('\nExporting data for provided RoachPass:')
+    print(roach.info)
+
+    out_dir = os.path.join(os.getcwd(), 'data', f'roach_{roach.id}_{roach.scan_pass.name.lower()}')
+    print('Created output directory: ', out_dir)
+
+    norm_df_file = os.path.join(out_dir, 'norm_df_dict')
+    np.save(norm_df_file, get_norm_df_dict(roach))
+    print('\nSaved normalized DF dict to: ', norm_df_file)
